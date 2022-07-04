9e1f2cc9d58610094b662e1156a00f1e6de1908c
Fabx <30447649+Fabxx@users.noreply.github.com>
2022-07-04 10:44:37 +0200

    Delete 6 - Textures and UV Mapping directory

--- 6 - Textures and UV Mapping/include/VertexArrayObjects.hh
+++
@@ -1,9 +0,0 @@
-#ifndef B0A9741D_2CC2_464E_872E_B60DC3956EA3
-#define B0A9741D_2CC2_464E_872E_B60DC3956EA3
-
-void VertexArrayObjects();
-void VertexUVMapping();
-void VertexArrayCompute();
-void VertexUVCompute();
-
-#endif /* B0A9741D_2CC2_464E_872E_B60DC3956EA3 */
--- 6 - Textures and UV Mapping/src/Zbuffer_check.cpp
+++
@@ -1,8 +0,0 @@
-#include "Zbuffer_check.hh"
-
-void check_zbuffer()
-{
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glEnable(GL_DEPTH_TEST);
-}
-    
