4044a56153cdc23ce13da9d4dce2467ed99ea53b
Fabx <30447649+Fabxx@users.noreply.github.com>
2022-07-05 20:41:09 +0200

    Update main.cpp

--- FreeGLUT Modern OpenGL Tutorials/5.1 Matrix - Rotation/src/main.cpp
+++ FreeGLUT Modern OpenGL Tutorials/5.1 Matrix - Rotation/src/main.cpp
@@ -19,13 +19,9 @@ static void RotationMatrix()
 	}
 
 	/** FOrmula Details:
-	 ** We take a 4x4 matrix and init to 1.0f for all values.
-	 ** we then pass the rotation matrix initialized, and we specify in degree how much we want to rotate
-	 ** Then, we choose on which axis we want to rotate, if XYZ.
-	 ** Here using 0.0, .1, or directly 1.0 gives the same result, since we're rotating with a 
-	 ** static value anyways. In this example, i'm rotating on the Z axis, but in fact, the shape rotates on X and Y
-	 ** axis, the Z remains constant, because of how the prespective works. Note that in this case rotating on X
-	 ** and Y axis will produce a blank screen, because this is not a 3D object, but a simple vertex.
+	 **The first line creates a new 4-by-4 matrix and initializes it to the identity matrix. 
+	 **The glm::rotate function multiplies this matrix by a rotation transformation of 180 degrees around the Z axis. 
+	 **Remember that since the screen lies in the XY plane, the Z axis is the axis you want to rotate points around.
 	 */
 	glm::mat4 RotationMatrix = glm::mat4(1.0f);
 	RotationMatrix = glm::rotate(RotationMatrix, glm::radians(AngleinRadians), glm::vec3(0.0f, 0.0f, 1.0f));
