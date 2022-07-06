a189a120eb5632b44fd7a3b2c62393f331d12d5d
Fabx <30447649+Fabxx@users.noreply.github.com>
2022-07-06 23:13:52 +0200

    Delete 6.1 Vertex Interpolation - Mix Attributes via Source code Buffer directory

--- 6.1 Vertex Interpolation - Mix Attributes via Source code Buffer/FragmentShader.glsl
+++
@@ -1,9 +0,0 @@
-#version 330 core
-
-in vec4 FragColor; //copying color from Vertex Shader.
-out vec4 interpolatedColors;
-
-void main()
-{
-  interpolatedColors = FragColor;
-}
--- 6.1 Vertex Interpolation - Mix Attributes via Source code Buffer/include/Buffer_Array_Object_Init.hh
+++
@@ -1,8 +0,0 @@
-#ifndef DA50E579_8E6F_48F8_BC00_2420CA0F2581
-#define DA50E579_8E6F_48F8_BC00_2420CA0F2581
-#include "Common_headers.hh"
-
-void CreateVertexBuffer();
-void CreateColorBuffer();
-
-#endif /* DA50E579_8E6F_48F8_BC00_2420CA0F2581 */
