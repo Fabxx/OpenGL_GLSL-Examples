85b49e97917c3f436e4f1662a5f49b54235dbc7f
Fabx <30447649+Fabxx@users.noreply.github.com>
2022-07-07 01:50:04 +0200

    Update RotationMatrix.cpp

--- GLFW Modern OpenGL Tutorials/4.1 Matrix - Rotation, rotating vertexes/src/RotationMatrix.cpp
+++ GLFW Modern OpenGL Tutorials/4.1 Matrix - Rotation, rotating vertexes/src/RotationMatrix.cpp
@@ -17,11 +17,13 @@ void RotationMatrix()
 	 **The first line creates a new 4-by-4 matrix and initializes it to the identity matrix. 
 	 **The glm::rotate function multiplies this matrix by a rotation transformation of "AngleinRadians" degrees around the Z axis. 
 	 **So the multiplication in this case is between (AngleInRadians multiplied by z axis).
-	 **Remember that since the screen lies in the XY plane, the Z axis is the axis you want to rotate points around.
-	 **When you rotate the object, you have to keep XY positions unaltered, and just rotate the object via Z direction. 
+	 **Remember that since the screen lies in the XY plane, the Z axis is the axis you want to rotate with a 2D object.
+	 **with a 3D object, you can rotate in all of the XYZ coordinates, BUT NOTE: YOU CAN ROTATE ONLY ON ONE AXIS PER TIME
+	 **(just like blender, you choose one rotation at time, DON'T BIND MULTIPLE ROTATION AXES AT THE SAME TIME.)
 	 **Draw a XY cartesian graph and add a Z axis in between,then draw a circle around the graph starting from the Z axis line, 
 	 **you'll understand this better. 
 	 */
+	
 	glm::mat4 RotationMatrix = glm::mat4(1.0f);
 	RotationMatrix = glm::rotate(RotationMatrix, glm::radians(AngleinRadians), glm::vec3(0.0f, 0.0f, 1.0f));
 
