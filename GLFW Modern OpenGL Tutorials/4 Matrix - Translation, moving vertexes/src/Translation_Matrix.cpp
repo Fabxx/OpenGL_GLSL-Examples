518ba4540888a2d6716d119ea2bb0ebf9af86e0a
Fabx <30447649+Fabxx@users.noreply.github.com>
2022-07-07 09:11:44 +0200

    Update Translation_Matrix.cpp

--- GLFW Modern OpenGL Tutorials/4 Matrix - Translation, moving vertexes/src/Translation_Matrix.cpp
+++ GLFW Modern OpenGL Tutorials/4 Matrix - Translation, moving vertexes/src/Translation_Matrix.cpp
@@ -19,21 +19,22 @@ void TranslationMatrix()
 
 	/** FOrmula Details:
 	 **The first line creates a new 4-by-4 matrix and initializes it to the identity matrix. 
-	 **The glm::translate function multiplies this matrix with the vec3 with "scale" coordinate around the Y axis. 
+	 **The glm::translate function multiplies this matrix with the vec3 with "scale" coordinate, in this case around the X and Y axis. 
 	 **Remember that since the screen lies in the XY plane, and this is a 2D object the XY axes are the axes you want to move the camera around.
-	 **If you work with a 3D object you can move on the Z axis too. If the value is positive it will go Down-leftm if it's negative  it will go up right
+	 **If you work with a 3D object you can move on the Z axis too. The values with glm::Translate have to be readed by COLUMN order. If readed by row, 
+	 **the positive XY values it will go Down-left, if are negative it will go up right.
 	 */
 
 	glm::mat4 ViewMatrix = glm::mat4(1.0f);
 	ViewMatrix = glm::translate(ViewMatrix, glm::vec3(scale, scale, 0.0f));
 
 	/**Gives matrix to shader, in detail:
-	 **glUniformMatrix4vf: gives to the shader a 4x4 matrix with float values
-	 **Matrix_Translation_Location updates the matrix in the shader, just like the uniform. 
-	 **1 number of matrixes that we send to the shader
-	 **GL_TRUE indicates if we give the values in ROW order or COLUMN order. So the read order of
+	 **-glUniformMatrix4vf: gives to the shader a 4x4 matrix with float values
+	 **-Matrix_Translation_Location updates the matrix in the shader, just like the uniform. 
+	 **-1 number of matrixes that we send to the shader
+	 **-GL_TRUE indicates if we give the values in ROW order or COLUMN order. So the read order of
 	 **the values changes with this orientation. FALSE reads by column, TRUE Reads by row.
-	 **Memory address of the Matrix array. tells the start point of where it should load the 4x4 float values.
+	 **-Memory address of the Matrix array. tells the start point of where it should load the 4x4 float values.
 	 **NOTE: Setting to FALSE since glm::translate inverts the X and Y axis
 	 */
 	glUniformMatrix4fv(Matrix_Translation_Location, 1, GL_FALSE, &ViewMatrix[0][0]);
