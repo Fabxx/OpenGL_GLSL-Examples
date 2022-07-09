a8f8340b456a958462157f2dda294b2a39988cd2
Fabx <30447649+Fabxx@users.noreply.github.com>
2022-07-09 12:10:09 +0200

    Add files via upload

---
+++ GLFW Modern OpenGL Tutorials/7 Z buffer and Culling check/src/main.cpp
@@ -0,0 +1,13 @@
+#include "Common_headers.hh"
+#include "Buffer_Array_Object_Init.hh"
+#include "Shader_Compiler.hh"
+#include "Display_Renderer_Init.hh"
+
+GLuint VBO;
+GLuint VAO; 
+
+int main(void)
+{
+	WindowInitRender();
+	return 0;
+}
