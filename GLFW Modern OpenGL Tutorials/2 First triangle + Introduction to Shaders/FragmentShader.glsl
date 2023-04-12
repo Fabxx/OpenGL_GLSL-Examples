6d9ef15315c8c67e423d201e159318000f0f99b1
Fabx <30447649+Fabxx@users.noreply.github.com>
2023-04-12 16:55:38 +0200

    Update FragmentShader.glsl

--- GLFW Modern OpenGL Tutorials/2 First triangle + Introduction to Shaders/FragmentShader.glsl
+++ GLFW Modern OpenGL Tutorials/2 First triangle + Introduction to Shaders/FragmentShader.glsl
@@ -2,7 +2,7 @@
 /**
  **  Note that when this fragment shader is called, this already knows the data from the VertexShader and which
  **  pixels to handle. IN this case, color will have RGBA values to color the pixels we've elaborated.
- ** the vector is declared as "out", meaning that from that vector will be outputted colored vertices 
+ ** the vector is declared as "out", meaning that from that vector will be output colored vertices 
  ** from VertexShader.glsl
  */
 out vec4 color;
